022d795a0d228c56c67523d7da6e946243131842
davitmelkonyan <dmelkony@uci.edu>
2021-11-14 09:10:23 -0800

    Update new_plot_manipulations.py
    suggestion 2

--- new_plot_manipulations.py
+++ new_plot_manipulations.py
@@ -4,3 +4,14 @@ def contradiction_repetition():
     # take entire plot 
     # get antonym for each and join(found wallet floor -> "lost purse ceiling" instead of "found purse wallet floor"
     # insert it to the front and back as a new plot (i.e. neg plot..but..pos plot..and..neg plot)
+    return
+
+
+def plot_manipulation_loop():
+    # do a subset of plot manipulations 
+    # get the new implausible output story
+    # extract new plots from it
+    # do manupulations on the new plots and repeat
+    return 
+
+
