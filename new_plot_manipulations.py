1af6251e530008c18d58f079f47c3a0b2efed446
davitmelkonyan <dmelkony@uci.edu>
2021-11-13 23:56:06 -0800

    Update new_plot_manipulations.py
    suggestion 1

---
+++ new_plot_manipulations.py
@@ -0,0 +1,6 @@
+import numpy as np 
+
+def contradiction_repetition():
+    # take entire plot 
+    # get antonym for each and join(found wallet floor -> "lost purse ceiling" instead of "found purse wallet floor"
+    # insert it to the front and back as a new plot (i.e. neg plot..but..pos plot..and..neg plot)
